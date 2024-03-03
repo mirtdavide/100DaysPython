a17e584faabbd63b94eda2df2feef5949a5b5617
mirtdavide <mirtdavide@gmail.com>
2024-03-03 20:04:15 +0200

    Day 1 Commit

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,9 @@
+
+#Simple program that generates a band name using information given by the user
+#The program asks the city the user grew up in, their pet's name and it generates a band name based on those
+#So basically it will save the names in two different variables and it will combine them together in the final print
+
+print("Welcome to the Band Name Generator.")
+city_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
