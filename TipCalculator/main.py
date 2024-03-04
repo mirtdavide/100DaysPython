00cfc6dfa65c9756f5218a47c1e8e6f8627066bc
mirtdavide <mirtdavide@gmail.com>
2024-03-04 16:38:48 +0200

    Day 2 Commit

---
+++ TipCalculator/main.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip calculator.")
+total_bill=float(input("What was the total bill? $"))
+people_number=int(input("How many people to split the bill? "))
+
+tip_percentage=int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+while(tip_percentage not in [10,12,15]):
+    tip_percentage = int(input("Wrong input,chose a percentage that is 10,12 or 15 "))
+
+total_plus_tip=total_bill + total_bill*(tip_percentage/100)
+total_per_person=round(total_plus_tip/people_number,2)
+print("Each person should pay: $",total_per_person)
+
+
